2e52684f3119b8f85b708fd923026af006e27fb2
Xiao Yav1 <1409951865@qq.com>
2024-12-02 14:22:40 +0800

    refactor(components): 优化组件代码结构
    - 重新格式化了 auth、drawer-side、head-end 和 header 组件的代码
    - 调整了缩进、空格和换行，提高了代码的可读性
    - 统一了图标组件的导入方式
    - 优化了部分变量和函数的定义

--- src/components/drawer-side.vue
+++ src/components/drawer-side.vue
@@ -1,62 +1,72 @@
 <template>
-    <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
-    <ul class="menu min-h-full w-60 p-4 shadow-sm bg-base-100 border-r-[1px] border-base-300">
-        <!-- Sidebar content here -->
-        <li v-for="item in menulist">
-            <a>
-                <component :is="item.icon" width="18"></component>
-                {{ item.name }}
-            </a>
+  <label
+    for="my-drawer-3"
+    aria-label="close sidebar"
+    class="drawer-overlay"
+  ></label>
+  <ul
+    class="menu min-h-full w-60 p-4 shadow-sm bg-base-100 border-r-[1px] border-base-300"
+  >
+    <!-- Sidebar content here -->
+    <li v-for="item in menulist">
+      <a>
+        <component :is="item.icon" width="18"></component>
+        {{ item.name }}
+      </a>
+    </li>
+
+    <h2 class="menu-title">功能</h2>
+
+    <li>
+      <a> <gptIcon width="18" />CHATGPT </a>
+    </li>
+
+    <h2 class="menu-title">记录</h2>
+
+    <div class="dropdown dropdown-end my-[2px]" v-for="i in 5">
+      <li class="parent">
+        <a :class="{ active: i === 1 }" class="parent">
+          <span></span>新会话
+
+          <button :tabindex="0" class="more-drop">
+            <moreDropIcon />
+          </button>
+        </a>
+      </li>
+      <ul
+        :tabindex="0"
+        class="dropdown-content w-max timeline-box bg-base-100 z-[1] p-2"
+      >
+        <li>
+          <a
+            >编辑
+            <editIcon width="18" />
+          </a>
         </li>
-
-        <h2 class="menu-title">功能</h2>
-
-        <li><a>
-                <gptIcon width="18" />CHATGPT
-            </a></li>
-
-        <h2 class="menu-title">记录</h2>
-
-        <div class="dropdown dropdown-end my-[2px] " v-for="i in 5">
-
-            <li class="">
-                <a :class="{ active: i === 1 }"> <span></span>新会话
-                    <button :tabindex="0">
-                        <moerDropIcon />
-                    </button>
-                </a>
-            </li>
-            <ul :tabindex="0" class="dropdown-content w-max timeline-box bg-base-100 z-[1] p-2">
-                <li><a>编辑
-                        <editIcon width="18" />
-                    </a></li>
-                <li><a class="!text-error">删除
-                        <deleteIcon width="18" />
-                    </a></li>
-
-            </ul>
-
-        </div>
-
-
-
-    </ul>
+        <li>
+          <a class="!text-error"
+            >删除
+            <deleteIcon width="18" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </ul>
 </template>
 
 <script setup lang="ts">
-import { reactive, markRaw } from 'vue'
-import envelopeIcon from '@/assets/icons/envelope.svg?component'
-import penIcon from '@/assets/icons/pen.svg?component'
-import gptIcon from '@/assets/icons/gpt.svg?component'
-import moerDropIcon from '@/assets/icons/more-drop.svg?component'
-import deleteIcon from '@/assets/icons/delete.svg?component'
-import editIcon from '@/assets/icons/edit.svg?component'
+import { reactive, markRaw } from "vue";
+import envelopeIcon from "@/assets/icons/envelope.svg?component";
+import penIcon from "@/assets/icons/pen.svg?component";
+import gptIcon from "@/assets/icons/gpt.svg?component";
+import moreDropIcon from "@/assets/icons/more-drop.svg?component";
+import deleteIcon from "@/assets/icons/delete.svg?component";
+import editIcon from "@/assets/icons/edit.svg?component";
 
 const menulist = reactive([
-    { name: '联系我', icon: markRaw(envelopeIcon) },
-    { name: 'BUG反馈', icon: markRaw(penIcon) },
-])
-
+  { name: "联系我", icon: markRaw(envelopeIcon) },
+  { name: "BUG反馈", icon: markRaw(penIcon) },
+]);
 </script>
 
 <style scoped></style>
